144569eb730fe718aaa42c83f87e7a19a7cc6038
aWildMixer <marnixjcvandenberg@gmail.com>
2024-12-27 16:20:45 +0100

    Update dextest.py
    Changed some prompts that the bot says

--- dextest.py
+++ dextest.py
@@ -31,15 +31,15 @@ async def spawn(ctx):
     print(random_card) #prints the integer to the console for troubleshooting purposes
     
     if  random_card <= 21:  # basicly the chance, in this instance if random choosen integer is lower than 21 then:
-        await ctx.send("You caught that Dicer guy!") #it sends a message that you caught the pokemon ;)
+        await ctx.send("You caught a wild Dicer!") #it sends a message that you caught the pokemon ;)
         
         # along with the message it will send the picture
-        embed = discord.Embed(title="Dicer Guy", description="Here's your catch!")
+        embed = discord.Embed(title="Dicer", description="Here's your catch!")
         embed.set_image(url="https://media.discordapp.net/attachments/1322202570529177642/1322205795957342228/CC-1947_2.png?ex=6770079b&is=676eb61b&hm=1d292e779b29d2258042788a7f8e0783e881dccd821baf36d3668a9c58d6dd&=&format=webp&quality=lossless&width=479&height=671")
         await ctx.send(embed=embed)
     
     else:
-        await ctx.send("You didn't catch anything, Loser.")
+        await ctx.send("You didn't catch anything, poor you...")
 
 
 # Run the bot, Bot code DO NOT SHARE!!!!! if you want to show someone the code online always use bot_code or something instead!
