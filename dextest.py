5f12955a168a2d0cd039c5a5b12af1c46857d593
aWildMixer <marnixjcvandenberg@gmail.com>
2024-12-28 14:01:12 +0100

    Update dextest.py
    Added logging to the console (will hopefully be very helpful)
    With logging, errors and weird things get reported to the console immediately.
    Also made a new start up cycle or idk

--- dextest.py
+++ dextest.py
@@ -1,12 +1,19 @@
 import discord # type: ignore
 import random
+import logging
 from discord.ext import commands # type: ignore
 from dotenv import load_dotenv
 import os
 
-# Load environment variables from .env file
+# Configuring the logging into the terminal
+logging.basicConfig(level=logging.INFO)
+
+# Loading the token from the .env file
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
+if not token: # If he can't find the token, error message and exit will occur
+    logging.error("DISCORD_TOKEN missing!") 
+    exit(1)
 
 # Bot Configuration, this holds what character you have to use to give the bot a command (in this case "!")
 intents = discord.Intents.default()
@@ -19,22 +26,22 @@ async def on_ready():
     print(f'We have logged in as {bot.user}')
 
 #gives you a random number
-@bot.command()
+@bot.command(name='random_number')
 async def randomNumber(ctx):
     random_number = random.randint(1, 150)
     await ctx.send(random_number)
 
 
 # Command: Respond to a simple message
-@bot.command()
+@bot.command(name='hello')
 async def hello(ctx):
     await ctx.send('Hello! I am your Discord bot.')
 
 #Command: basicly coded for chances of getting cards. Horribly coded, FIRST THING TO CHANGE IF IT IS DONE!!!!! 
-@bot.command()
+@bot.command(name='spawn')
 async def spawn(ctx):
     random_card = random.randint(1, 200) #chooses a random integer 
-    print(random_card) #prints the integer to the console for troubleshooting purposes
+    logging.info(f'Generated random card number: {random_card}') # prints the integer to the console for troubleshooting purposes
     
     if  random_card <= 21:  # basicly the chance, in this instance if random choosen integer is lower than 21 then:
         await ctx.send("You caught a wild Dicer!") #it sends a message that you caught the pokemon ;)
@@ -43,10 +50,12 @@ async def spawn(ctx):
         embed = discord.Embed(title="Dicer", description="Here's your catch!")
         embed.set_image(url="https://media.discordapp.net/attachments/1322202570529177642/1322205795957342228/CC-1947_2.png?ex=6770079b&is=676eb61b&hm=1d292e779b29d2258042788a7f8e0783e881dccd821baf36d3668a9c58d6dd&=&format=webp&quality=lossless&width=479&height=671")
         await ctx.send(embed=embed)
-    
     else:
         await ctx.send("You didn't catch anything, poor you...")
 
-
-# Run the bot, located in .env (if everythink ok?) STILL DO NOT SHARE
-bot.run(token)
+# Ensures that it will only work when executed directly, and will log any errors to the terminal
+if __name__ == "__main__":
+    try:
+        bot.run(token)
+    except Exception as e:
+        logging.error(f"Error running the bot: {e}")
