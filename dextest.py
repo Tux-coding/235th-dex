3df40195691536c960e7fdfdd838349c56ab931e
aWildMixer <marnixjcvandenberg@gmail.com>
2024-12-28 16:17:57 +0100

    Added second card, Reyes, to the deck without an image.  Will add image later.

--- dextest.py
+++ dextest.py
@@ -45,11 +45,16 @@ async def spawn(ctx):
     logging.info(f'Generated random card number: {random_card}') # prints the integer to the console for troubleshooting purposes
     
     if  random_card <= 21:  # basicly the chance, in this instance if random choosen integer is lower than 21 then:
-        await ctx.send("You caught a wild Dicer!") #it sends a message that you caught the pokemon ;)
+        await ctx.send("You caught a wild Dicer!") # it sends a message that you caught the pokemon ;)
         
         # along with the message it will send the picture
         embed = discord.Embed(title="Dicer", description="Here's your catch!")
         embed.set_image(url="https://media.discordapp.net/attachments/1322202570529177642/1322205795957342228/CC-1947_2.png?ex=6770079b&is=676eb61b&hm=1d292e779b29d2258042788a7f8e0783e881dccd821baf36d3668a9c58d6dd&=&format=webp&quality=lossless&width=479&height=671")
+        await ctx.send(embed=embed)
+    elif random_card >= 179 and random_card <= 190: #if the random number is higher than 179 and lower than 190 then:
+        await ctx.send("You caught a wild Reyes!") # it sends a message that you caught the pokemon ;)
+        embed = discord.Embed(title="Reyes", description="Here's your catch!")
+        # embed.set_image(url="https://media.discordapp.net/attachments/1322202570529177642/1322205795957342228/CC-1947_2.png?ex=6770079b&is=676eb61b&hm=1d292e779b29d2258042788a7f8e0783e881dccd821baf36d3668a9c58d6dd&=&format=webp&quality=lossless&width=479&height=671") 
         await ctx.send(embed=embed)
     else:
         await ctx.send("You didn't catch anything, poor you...")
