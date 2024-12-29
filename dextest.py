65daf87d279d55dfed5b02c469f53a1698d743c7
aWildMixer <marnixjcvandenberg@gmail.com>
2024-12-29 13:23:56 +0100

    Attempt #1 at new spawning mechanic

--- dextest.py
+++ dextest.py
@@ -52,12 +52,26 @@ class CatchView(View):
         super().__init__(timeout=None)
         self.add_item(CatchButton())
 
+# NEW: ALL CARDS IN THE FOLLOWING FORMAT
+cards = [
+    {
+        "name": "Dicer",
+        "image_url": "https://media.discordapp.net/attachments/1322202570529177642/1322205795957342228/CC-1947_2.png?ex=6770079b&is=676eb61b&hm=1d292e779b29d2258042788a7f8e0783e881dccd821baf36d3668a9c58d6dd&=&format=webp&quality=lossless&width=479&height=671"
+    },
+    {
+        "name": "Reyes",
+        "image_url": "https://media.discordapp.net/attachments/1321821231850328068/1322584515146678335/CC-1598_Reyes_is_the_Marshal_Commander_of_the_235th_Elite_Corps._He_has_led_the_Corps_since_the_very_beginning_although_the_highest_rank_was_Senior_Commander_back_in_the_day._Known_for_his_steady.png?ex=67716850&is=677016d0&hm=c9811709d3f40e4f08220a9e1f553eeac4e89b32608473a88df31e646b6450a5&=&format=webp&quality=lossless&width=479&height=671"
+    }
+]
 # Part that holds the timer and the channel where the card spawns
 @tasks.loop(minutes=10)
 async def spawn_card():
     try:
         channel = bot.get_channel(int(channel_id))
         if channel:
+            card = random.choice(cards)
+            embed = discord.Embed(title="A wild card has appeared!", description="Click the button below to catch it!")
+            embed.set_image(url=card["image_url"])
             await channel.send("A wild card has appeared! Click the button below to catch it!", view=CatchView())
         else:
             logging.error(f"Channel not found: {channel_id}")
@@ -102,7 +116,7 @@ async def spawn(ctx):
         player_cards[user_id] = []
     
     if  random_card <= 21:  # basicly the chance, in this instance if random choosen integer is lower than 21 then:
-        embed = discord.Embed(title = "mystery man", description ="Who is this?")      
+        embed = discord.Embed(title = "Dicer", description ="Who is this?")      
         embed.set_image(url="https://media.discordapp.net/attachments/1322205679028670495/1322205863611600896/RobloxScreenShot20241227_145936188.png?ex=6771592b&is=677007ab&hm=dce13f31e721d625b43ac537d5d17b8e3dc5158d67a9035f59b5d4971bfe8502&=&format=webp&quality=lossless&width=650&height=667")
         await ctx.send(embed=embed)
         
