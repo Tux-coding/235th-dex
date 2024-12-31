5242e510fd6eff0edd47142a8d949ac4c3dc1921
aWildMixer <marnixjcvandenberg@gmail.com>
2024-12-31 08:31:48 +0100

    bugfix

--- dextest.py
+++ dextest.py
@@ -138,7 +138,8 @@ class ProgressView(View):
         await self.update_message(interaction)
 
     async def on_quit(self, interaction: discord.Interaction):
-        await interaction.delete_original_response()
+        await interaction.response.send_message("Closing progress view...", delete_after=1)
+        await interaction.message.delete()
 
     async def update_message(self, interaction: discord.Interaction):
         start = self.page * self.items_per_page
