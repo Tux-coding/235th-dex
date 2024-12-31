b3bcf9ad671df0e16835fae22197d7e769dd28c2
Tux-coding <162804430+Tux-coding@users.noreply.github.com>
2024-12-31 22:52:43 +0100

    some little improvements nothing major, info changed to list_commands
    -removed some unnececary description of the current commands, due to issues with scaling on mobile
    -added more variables to the cards (health, attack(currently called damage)) for if we want to add battling later down the line
    -along with a command called "print_stats  card name" 
    -switched command "info" to "list_commands" 
    -added command "info" to display the current release

--- dextest.py
+++ dextest.py
@@ -176,79 +176,105 @@ cards = [
         "name": "Dicer",
         "spawn_image_url": "https://media.discordapp.net/attachments/1322205679028670495/1322205863611600896/RobloxScreenShot20241227_145936188.png?ex=677201eb&is=6770b06b&hm=f74d9e756dd8ae25b0c65b6fbdfd169c1c08679912c956c068f75c50171e9b95&=&format=webp&quality=lossless",
         "card_image_url": "https://cdn.discordapp.com/attachments/1322202570529177642/1322205795957342228/CC-1947_2.png?ex=6773535b&is=677201db&hm=768dee2d6535855233f44910ff8dd7ca45485ca6ba81a6f36ec7f3a554bd3941&",
-        "rarity": 5  # 5% chance of spawning
+        "rarity": 5,  # 5% chance of spawning
+        "health": 6500,
+        "damage": 3500,
     },
     {
         "name": "Reyes",
         "spawn_image_url": "https://media.discordapp.net/attachments/1322205679028670495/1322503928591548482/RobloxScreenShot20241228_105546001.png?ex=67726ec3&is=67711d43&hm=4978a7aa74371952a439ec75fd2ac459bb1f49af739b8ec7d0f6d145c54642d9&=&format=webp&quality=lossless",
         "card_image_url":"https://cdn.discordapp.com/attachments/1322202570529177642/1322585956938944512/CC-1598_Reyes_is_the_Marshal_Commander_of_the_235th_Elite_Corps.png?ex=677363e8&is=67721268&hm=35ffd80d26b26c28e60db3da255a071919fa7f0e87c1d8e2aecdd75e65e5031b&",
-        "rarity": 0.5
+        "rarity": 0.5,
+        "health": 10000,
+        "damage": 5000,
     },
     {
         "name": "Sentinel",
         "spawn_image_url": "https://media.discordapp.net/attachments/1322205679028670495/1322284693164523520/RobloxScreenShot20241227_202621517.png?ex=67724b55&is=6770f9d5&hm=79e9f8374ffa7b0138f938a11d7916b45ee79b2afdf1fc6c8197c27349b46684&=&format=webp&quality=lossless&width=377&height=350",
         "card_image_url": "https://media.discordapp.net/attachments/1322202570529177642/1323317997414121665/Just_another_trooper_trying_to_stay_alive._He_doesnt_like_insurgents._His_left_arm_and_left_eye_are_gone_due_to_the_Gulag.png?ex=6774136c&is=6772c1ec&hm=f4d4550607203a12eaa72d7b6a0086efb64bf3d8c623650d713dbf7a96bc242d&=&format=webp&quality=lossless&width=479&height=671",
-        "rarity": 23  
+        "rarity": 23,
+        "health": 1650,
+        "damage": 550,
     },
     {
         "name": "Blau",
         "spawn_image_url": "https://media.discordapp.net/attachments/1322205679028670495/1322289394404687957/RobloxScreenShot20241227_204520218.png?ex=67724fb6&is=6770fe36&hm=b3fc9388163dfb8f6d33ea520cccc373156a6ea8c5b558d370d388a8ec8ae7d8&=&format=webp&quality=lossless&width=396&height=350",
         "card_image_url": "https://cdn.discordapp.com/attachments/1322202570529177642/1322289134630600724/CT-_Blau.png?ex=6773a0f8&is=67724f78&hm=29349d7e624fcb150f298c40b15d68575d5bee22dace451856ea9da23e43db58&",
-        "rarity": 12.5 
+        "rarity": 12.5,
+        "health": 6050,
+        "damage": 2250,
     },
     {
         "name": "Hounder",
         "spawn_image_url": "https://media.discordapp.net/attachments/1322205679028670495/1322295423360176218/RobloxScreenShot20241227_210852940.png?ex=67725553&is=677103d3&hm=c32cad3bc0dc2ebc26cbdfeba7e8fb5ea83b55e557eafbfc3a0358a760654d7e&=&format=webp&quality=lossless",
         "card_image_url": "https://cdn.discordapp.com/attachments/1322202570529177642/1322293233815912498/CT-_Blau_1.png?ex=6773a4c9&is=67725349&hm=e37704c3fa30bc469f1b20c7bd7e18e25abe8c1d6bc06b1cbcd3b85d359d2c10&",
-        "rarity": 9.5  
+        "rarity": 9.5,
+        "health": 9250,
+        "damage": 4500,
     },
     {
         "name": "Pipopro",
         "spawn_image_url": "https://media.discordapp.net/attachments/1322205679028670495/1322977622778576976/artworks-OF3WFPHsapfnZrW8-lDiZAQ-t500x500.png?ex=67737f2d&is=67722dad&hm=1ec22c5b9af4c994af583de216afe05660c777e5437a738750577b9a49cbdf90&=&format=webp&quality=lossless",
         "card_image_url": "https://cdn.discordapp.com/attachments/1322202570529177642/1322977702105579591/Pipopro.png?ex=67737f3f&is=67722dbf&hm=2cc3c639f2e613963f78ec6dbfc274df6f6a74666d06461e4bfb60fdb5c3568d&",
-        "rarity": 0.75 
+        "rarity": 0.75,
+        "health": 15000,
+        "damage": 7500,
     },
     {
         "name": "Wilson",
         "spawn_image_url": "https://media.discordapp.net/attachments/1322205679028670495/1323344360879689730/RobloxScreenShot20241230_161947936.png?ex=67742bfa&is=6772da7a&hm=14a472045ae47f536705b91cb91bdae7ea52674d1cde2799ad65835c0c85d8df&=&format=webp&quality=lossless&width=532&height=350",
         "card_image_url": "https://media.discordapp.net/attachments/1322202570529177642/1323314295286665246/CC-.png?ex=67740ff9&is=6772be79&hm=54193da49212bd838fb47e993c16c2af1da3987a04373fc1a20def924f0744a6&=&format=webp&quality=lossless&width=479&height=671",
-        "rarity": 1 
+        "rarity": 1,
+        "health": 9550,
+        "damage": 4670,
     },
     {
         "name": "Stinger",
         "spawn_image_url": "https://media.discordapp.net/attachments/1322205679028670495/1323379787766698116/RobloxScreenShot20241230_205740938.png?ex=67744cf8&is=6772fb78&hm=ffe51d5793e02353736175578a9a6158e243b285a65649e695ba924a710c3c83&=&format=webp&quality=lossless&width=375&height=350",
         "card_image_url": "https://media.discordapp.net/attachments/1322202570529177642/1323383213451640862/Just_another_trooper_trying_to_stay_alive._He_doesnt_like_insurgents._His_left_arm_and_left_eye_are_gone_due_to_the_Gulag_13.png?ex=67745029&is=6772fea9&hm=c08cafd74fea97dcd3c7c6847ab940b55249ed62f0585149ca03e05c13ed6ea7&=&format=webp&quality=lossless&width=479&height=671",
-        "rarity": 75 
+        "rarity": 75,
+        "health": 2150,
+        "damage": 950,
     },
     {
         "name": "Sandy",
         "spawn_image_url": "https://media.discordapp.net/attachments/1322205679028670495/1323386530303053854/RobloxScreenShot20241230_211843002.png?ex=67745340&is=677301c0&hm=1679defda16cd577a0146f6dc1ce3e6e9bb08eac3934ecb6512a4a7711a34c09&=&format=webp&quality=lossless&width=522&height=350",
         "card_image_url": "https://media.discordapp.net/attachments/1322202570529177642/1323386408550928404/Just_another_trooper_trying_to_stay_alive._He_doesnt_like_insurgents._His_left_arm_and_left_eye_are_gone_due_to_the_Gulag_14.png?ex=67745323&is=677301a3&hm=dd52bd5a4777cc90d0f8a6793251192ac14a526e55037f3d2d32bb0db691471c&=&format=webp&quality=lossless&width=479&height=671",
-        "rarity": 50 
+        "rarity": 50,
+        "health": 3000,
+        "damage": 1950,
     },
     {
         "name": "Rancor",
         "spawn_image_url": "https://media.discordapp.net/attachments/1322205679028670495/1323561918878847047/RobloxScreenShot20241231_090056606.png?ex=6774f698&is=6773a518&hm=cffc47d9ab08a0b3d98df85ef86f7abf413be74c2b4f2ecc56ee520991ed5f7d&=&format=webp&quality=lossless",
         "card_image_url": "https://media.discordapp.net/attachments/1322202570529177642/1323563829925380166/Just_another_trooper_trying_to_stay_alive._He_doesnt_like_insurgents._His_left_arm_and_left_eye_are_gone_due_to_the_Gulag_15.png?ex=6774f85f&is=6773a6df&hm=d65e4c7ce98eb87dd896234a15a4a9d742d83fd44292fe1b0b392938a7bce01b&=&format=webp&quality=lossless&width=479&height=671",
-        "rarity": 12.5 
+        "rarity": 12.5,
+        "health": 6950,
+        "damage": 3050,
     },
     {
         "name": "Cooker",
         "spawn_image_url": "https://media.discordapp.net/attachments/1322205679028670495/1323570324767379497/RobloxScreenShot20241231_093357795.png?ex=6774fe6c&is=6773acec&hm=c75865e285d3d39f4a6476d442a01c9e1dbe3611a0b06cbd862c8c43b770e956&=&format=webp&quality=lossless&width=468&height=350",
         "card_image_url": "https://media.discordapp.net/attachments/1322202570529177642/1323573966652309504/Just_another_trooper_trying_to_stay_alive._He_doesnt_like_insurgents._His_left_arm_and_left_eye_are_gone_due_to_the_Gulag_17.png?ex=677501d0&is=6773b050&hm=8fe64e466f95254086fd222ad3698819eff00c38d7ab2333f132458ca25af856&=&format=webp&quality=lossless&width=479&height=671",
-        "rarity": 17.5 
+        "rarity": 17.5,
+        "health": 5000,
+        "damage": 2500,
     },
     {
         "name": "Longshot",
         "spawn_image_url": "https://media.discordapp.net/attachments/1322205679028670495/1323671916631949362/RobloxScreenShot20241231_161614848.png?ex=67755d09&is=67740b89&hm=cab4a3d1dba4eb57b42e592905780faaa24d5ffba9746ce3406325c7f520ef0d&=&format=webp&quality=lossless&width=480&height=350",
         "card_image_url": "https://media.discordapp.net/attachments/1322202570529177642/1323671828111298584/Just_another_trooper_trying_to_stay_alive._He_doesnt_like_insurgents._His_left_arm_and_left_eye_are_gone_due_to_the_Gulag_19.png?ex=67755cf4&is=67740b74&hm=05d5e4c11313875aea75d4c7e227bf32546af435d3755ebac0e66d817c11d2f5&=&format=webp&quality=lossless&width=479&height=671",
-        "rarity": 70
+        "rarity": 70,
+        "health": 2000,
+        "damage": 650,
     },
     {
         "name": "Mertho",
         "spawn_image_url": "https://media.discordapp.net/attachments/1322205679028670495/1323578208863649863/RobloxScreenShot20241231_100652348.png?ex=677505c3&is=6773b443&hm=e8aad1e00d28942444ea357afa5b4ce2d0d4cd1949ad84e29fb99aae522be1f8&=&format=webp&quality=lossless&width=384&height=350",
         "card_image_url": "https://media.discordapp.net/attachments/1322202570529177642/1323672693647609937/Just_another_trooper_trying_to_stay_alive._He_doesnt_like_insurgents._His_left_arm_and_left_eye_are_gone_due_to_the_Gulag_21.png?ex=67755dc2&is=67740c42&hm=ddb0d3ac0879577f7497b50ccf367d6fe0afe12850451350bf7b79528dd56b49&=&format=webp&quality=lossless&width=479&height=671",
-        "rarity": 70
+        "rarity": 70,
+        "health": 1700,
+        "damage": 500,
     }
 ]
 
@@ -279,7 +305,7 @@ async def set_spawn_mode(ctx, mode: str):
 # Part that holds the timer and the channel where the card spawns
 last_spawned_card = None
 
-@tasks.loop(minutes=1)
+@tasks.loop(minutes=5)
 async def spawn_card():
     global last_spawned_card
     try:
@@ -318,6 +344,21 @@ async def spawn_card():
         for channel in channels:
             await channel.send("An error occurred while spawning a card.")
 
+# Command to print the stats of a card
+@bot.command(name='print_stats')
+async def print_stats(ctx, card_name: str):
+    card_name = card_name.strip().lower()
+    card = next((card for card in cards if card["name"].lower() == card_name), None)
+    if card:
+        embed = discord.Embed(title=f"Stats for {card['name']}", description="")
+        embed.add_field(name="Health", value=card["health"], inline=True)
+        embed.add_field(name="Damage", value=card["damage"], inline=True)
+        embed.add_field(name="Rarity", value=f"{card['rarity']}%", inline=True)
+        await ctx.send(embed=embed)
+    else:
+        await ctx.send("Card not found.")
+
+
 # If error, he says why
 @bot.event
 async def on_command_error(ctx, error):
@@ -336,6 +377,7 @@ async def on_ready():
     print(f'We have logged in as {bot.user}')
     logging.info("Logging is configured correctly.")
     spawn_card.start()
+
 
 # Command that hopefully sees your cards
 @bot.command(name='mycards')
@@ -439,21 +481,34 @@ async def random_number(ctx):
     random_number = random.randint(0, 10000000)
     await ctx.send(f'Your random number is: {random_number}')
 
-# Info command
-# Info command
-@bot.command(name='info')
-async def info(ctx):
+
+# command to show the current commands that users can use
+@bot.command(name='list_commands')
+async def list_commands(ctx):
     commands_list = [
         '!hello - Responds with a greeting message.',
-        '!random_number - Gives a random number between 0 and 10000000.',
-        '!info - Shows this list of commands.',
+        '!random_number - Gives a random number',
+        '!info - shows the current release ',
         '!mycards - Shows the cards you have caught.',
-        '!see_card - Allows you to see a specific card you have caught.',
-        '!progress - Shows your progress in catching cards.'
+        '!see_card - View a card you have caught.',
+        '!progress - Shows your progress in catching cards.',
+        '!commands - Shows a list of all the commands you can use.',   
     ]
     commands_description = '\n'.join(commands_list)
-    await ctx.send(f'Here are a list of all the commands you can use:\n{commands_description}')
+    await ctx.send(f'Here is a list of all the commands you can use:\n{commands_description}')
 
+#info, command to show the current release
+@bot.command(name='info')
+async def info(ctx):
+    await ctx.send('Current release: beta') #expand later when we actually released the bot to the public
+
+
+
+
+
+
+#///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#other cool things for shutdown and signal handling
 # Custom shutdown function
 async def shutdown_bot():
     channels = [bot.get_channel(int(channel_id)), bot.get_channel(int(test_channel_id))]
